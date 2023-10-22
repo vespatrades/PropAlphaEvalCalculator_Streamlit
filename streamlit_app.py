84fd5e17dc394bcd753318416f0f6416b18f50c5
vespatrades <vespatrades@nosuchthing.org>
2023-10-22 16:24:32 -0500

    Initial commit of PropAlphaEvalCalculator as Streamlit app
    Modify the calculator from https://github.com/Prop-Alpha/PropAlphaEvalCalculator
    to be a Streamlit app so it can be deployed as a web application.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,76 @@
+# This based on calculator from https://github.com/Prop-Alpha/PropAlphaEvalCalculator
+
+import math
 import streamlit as st
+from winloss_graph import WinLossDiGraph
+from PIL import Image
 
-st.title('🎈 App Name')
+def calculate_probability(trailing_dd, account_target, stop_width, tp_width, win_pct):
+    num_loss = math.ceil(trailing_dd / stop_width)
+    num_win = math.ceil(account_target / stop_width)
+    rr_ratio = round(tp_width / stop_width, 1)
+    win_frac = win_pct / 100.0
 
-st.write('Hello world!')
+    wldg = WinLossDiGraph()
+    wldg.build_graph_from_start_node_and_params(win_frac, rr_ratio, num_win, num_loss)
+    wldg.generate_adj_matrix()
+    win_prob = wldg.get_win_prob_from_start_node() * 100
+
+    return f"Estimated Probability of Success: {win_prob:.1f}%"
+
+
+def run():
+    image = Image.open('propalpha_logo.png')
+
+    col1, col2, col3 = st.columns([6, 6, 6])
+
+    with col1:
+        st.write("")
+    with col2:
+        st.image(image, width=100)
+    with col3:
+        st.write("")
+
+
+    st.title("Prop Alpha Eval Calculator")
+
+    st.markdown("""
+        Estimate odds of passing a prop eval with trailing drawdown given a single setup with a defined bracket and win percentage.\n
+        Costs ignored.\n
+        NOT FINANCIAL ADVICE. DO YOUR OWN RESEARCH. NO GUARANTEE OUR MATH IS CORRECT.\n
+        RISK DISCLAIMER: [Read Here](https://www.prop-alpha.com/disclaimer)\n
+    """)
+
+    presets = {
+        "Custom": {"trailing_dd": 0.0, "account_target": 0.0},
+        "Apex 50k": {"trailing_dd": 2500.0, "account_target": 3000.0},
+        "Apex 150k": {"trailing_dd": 5000.0, "account_target": 9000.0},
+        "Apex 300k": {"trailing_dd": 7500.0, "account_target": 20000.0}
+    }
+
+    selected_preset = st.selectbox("Choose a preset or select Custom:", list(presets.keys()))
+
+    with st.container():
+        trailing_dd = st.number_input(
+            "Enter Trailing Drawdown Amount in Currency",
+            value=presets[selected_preset]["trailing_dd"],
+            step=100.0
+        )
+        account_target = st.number_input(
+            "Enter Account Target Amount in Currency",
+            value=presets[selected_preset]["account_target"],
+            step=100.0
+        )
+        stop_width = st.number_input("Enter Stop Size in Currency", value=0.0, step=100.0)
+        tp_width = st.number_input("Enter Take Profit Size in Currency", value=0.0, step=100.0)
+        win_pct = st.number_input("Enter Estimated Win Percent", value=0.0)
+
+    compute_button = st.button("Compute Estimated Probability")
+
+    if compute_button:
+        result = calculate_probability(trailing_dd, account_target, stop_width, tp_width, win_pct)
+        st.markdown(f"### {result}")
+
+
+if __name__ == '__main__':
+    run()
